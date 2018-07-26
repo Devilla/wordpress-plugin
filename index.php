332aee132f9acb3c61348033a80910c0faa1164e
devilla <dev.koold@gmail.com>
2018-07-26 17:40:36 +0530

    influence plugin GUI

--- index.php
+++ index.php
@@ -1,1 +1,23 @@
-<?php // Silence is golden
+<?php
+/*Plugin name : Influence Plugin
+Version: 1.0
+Description: WP plugin and TrackingId Input
+Author: Devilla
+Plugin URI: https://useinfluence.co/
+*/
+
+add_action('admin_menu', 'basicPluginMenu');
+
+function basicPluginMenu(){
+  $appName = 'Influence Plugin';
+  $appID = 'basic-plugin';
+  add_menu_page($appName, $appName, 'administrator', $appID . '-top-level', 'pluginAdminScreen');
+}
+
+function pluginAdminScreen() {
+  echo "<h1>The Influence Plugin Admin Area</h1>";
+  echo "<p>Please enter your TrakingID</p>";
+  echo "<input type='text'></input>";
+}
+
+?>
