fe854612195f1d647bea2861b766d50445f81716
Devilla <dev.koold@gmail.com>
2018-08-11 15:44:18 +0530

    Update useinfluence.php

--- useinfluence.php
+++ useinfluence.php
@@ -114,9 +114,10 @@ function influence_screen() {
 	echo "</h3>";
 	echo "<h2>Please enter your Tracking ID</h2>";
 	echo "<form action='' method='POST'>";
-  echo "<input type='text' name='trackingId' class='api' style='padding: 5px 10px; border-radius:5px;' placeholder='e.g. INF-xxxxxxxx'></input>";
+        echo "<input type='text' name='trackingId' class='api' style='padding: 5px 10px; border-radius:5px;' placeholder='e.g. INF-xxxxxxxx'></input>";
 	echo "<br /> <hr />";
 	echo "<input type='submit' class='submit' style='padding: 5px 10px ;cursor:pointer; color:#fff; border-radius:5px;background-color:#097fff' value='Save'></input>";
+	echo "<a href='https://useinfluence.co/campaigns/scripts' target='_blank'>Where is my Tracking ID ?</a>"
 	echo "<form>";
 
  	 global $trackingId;
